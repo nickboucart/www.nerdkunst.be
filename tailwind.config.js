5c708890f30c9d82b5c6a1e445affc35ea143101
Nick Boucart <nboucart@gmail.com>
2024-10-21 09:39:31 +0200

    daisyUI plus retro theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ export default {
   },
   plugins: [
     require('@tailwindcss/typography'),
+    require('daisyui'),
   ],
+  daisyui: {
+    themes: ["retro"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+
+  },
 }
 

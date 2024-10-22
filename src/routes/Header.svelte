7f06519ea79bd4a2c8193209f5a3a91586460d5b
Nick Boucart <nboucart@gmail.com>
2024-10-22 11:23:45 +0200

    header is nu wel klaar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -27,8 +27,8 @@
 >
 	<div class="hero-overlay bg-opacity-60"></div>
 	<div class="hero-content text-neutral-content text-center">
-		<div class="max-w-md ">
-			<h1 class="mb-5 text-8xl font-bold">{title}</h1>
+		<div class="max-w-lg ">
+			<h1 class="mb-5 text-5xl font-bold">{title}</h1>
 			<p class="mb-5 text-2xl">
 				{description}
 			</p>

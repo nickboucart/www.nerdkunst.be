06a04136742df07e298a86bdbfec004ed139440b
Nick Boucart <nboucart@gmail.com>
2024-12-07 08:35:06 +0100

    het runnen van code is mooier nu

--- src/lib/components/P5.svelte
+++ src/lib/components/P5.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
     import { url } from '$lib/config'
+    import { CirclePlay, CirclePause} from 'lucide-svelte';
     import { CodeBlock } from 'svhighlight';
     import 'highlight.js/styles/monokai-sublime.css';
     export let code;
     export let htmlPage: string = "index.html";
+    let name = Math.random().toString(36).substring(2,7);
+
+    let getUrlForReadyIframe = () => {
+        return new URL('/p5/ready/index.html', url).toString();
+    }
 
 
     let getEntireUrlForIframe = (sketch: string, html: string) => {
@@ -19,6 +25,10 @@
 <div class="md:-mx-32 grid grid-cols-1 md:grid-cols-2 justify-center content-evenly not-prose">	
     <CodeBlock {code} language="javascript" dimensions="w-[600px]" />
     <div class="aspect-square w-[600px] mt-2">
-    <iframe loading="lazy" class="aspect-square w-[600px] mt-2"  title="p5 sketch" src={getEntireUrlForIframe(code, htmlPage)} />
+        <div class="flex flex-row gap-2">
+            <a class="btn" href="{getEntireUrlForIframe(code, htmlPage)}" target={name}><CirclePlay /></a>
+            <a class="btn" href="{getUrlForReadyIframe()}" target={name}><CirclePause /></a>
+        </div>
+    <iframe loading="lazy" class="aspect-square w-[600px] mt-2" {name} title="p5 sketch" src={getUrlForReadyIframe()} />
 </div>
 </div>

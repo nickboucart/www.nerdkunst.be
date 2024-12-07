c276aa2137a941e1085556fb69b328d11b168892
Nick Boucart <nboucart@gmail.com>
2024-12-07 11:29:40 +0100

    codemirror integratie plus opkuis eerdere coding dingen

--- src/lib/components/P5.svelte
+++ src/lib/components/P5.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import { url } from '$lib/config'
     import { CirclePlay, CirclePause} from 'lucide-svelte';
-    import { CodeBlock } from 'svhighlight';
-    import 'highlight.js/styles/monokai-sublime.css';
-    export let code;
+    import CodeMirror from "svelte-codemirror-editor";
+    import { javascript } from "@codemirror/lang-javascript";
+    import { cobalt } from "thememirror";
+
+  
+    export let code: string;
     export let htmlPage: string = "index.html";
     let name = Math.random().toString(36).substring(2,7);
 
@@ -22,13 +25,17 @@
 </script>
 
 
-<div class="md:-mx-32 grid grid-cols-1 md:grid-cols-2 justify-center content-evenly not-prose">	
-    <CodeBlock {code} language="javascript" dimensions="w-[600px]" />
-    <div class="aspect-square w-[600px] mt-2">
-        <div class="flex flex-row gap-2">
+<div class="grid grid-cols-1 lg:grid-cols-2 justify-center content-evenly not-prose gap-4">	
+    <CodeMirror bind:value={code} lang={javascript()} theme={cobalt} class="not-prose text-sm" styles={{
+        "&": {
+            height: "30rem",
+        },
+    }}/>
+    <div class="aspect-square">
+        <div class="flex flex-row gap-5 bg-base-200">
             <a class="btn" href="{getEntireUrlForIframe(code, htmlPage)}" target={name}><CirclePlay /></a>
             <a class="btn" href="{getUrlForReadyIframe()}" target={name}><CirclePause /></a>
         </div>
-    <iframe loading="lazy" class="aspect-square w-[600px] mt-2" {name} title="p5 sketch" src={getUrlForReadyIframe()} />
+    <iframe loading="lazy" class="aspect-square mt-2 w-full mx-auto" {name} title="p5 sketch" src={getUrlForReadyIframe()} />
 </div>
 </div>

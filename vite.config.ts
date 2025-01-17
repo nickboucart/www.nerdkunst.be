bc41c7984aa7d0adb7f0af5121a11e2ae7123d15
Nick Boucart <nboucart@gmail.com>
2025-01-17 20:20:23 +0100

    strange troubles with codemirror and no need to load js libs in the ready page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	optimizeDeps: {
         exclude: ["svelte-codemirror-editor", "codemirror", "@codemirror/language-javascript" /* ... */],
     },
+    resolve: {
+        alias: {
+          '@codemirror/state': path.resolve(__dirname, 'node_modules/@codemirror/state/dist/index.js'),
+        }
+    }
 });
